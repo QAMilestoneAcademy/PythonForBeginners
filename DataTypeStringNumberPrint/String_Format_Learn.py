f45d8955f5a30e0610cfbcb7220f3bf9b20aed45
QAMilestoneAcademy <48306511+QAMilestoneAcademy@users.noreply.github.com>
2020-08-05 07:49:59 +0400

    commit

---
+++ DataTypeStringNumberPrint/String_Format_Learn.py
@@ -0,0 +1,10 @@
+#String format - Freedom to create sentence
+product="Table"
+price = 300
+totalMoney = 900
+quantity = 3
+
+print("My product is {}."
+      "Price of each table is {}."
+      "I have total money {}."
+      "Hence I can bring {} number of product {}".format(product,price,totalMoney,quantity,product))
